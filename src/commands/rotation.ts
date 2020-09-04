da22d9792fcf1370840072c712a914797b95b7a4
Heejae Kwon <hjkwon93@kakao.com>
2020-09-04 13:16:44 +0900

    hide apikey

--- src/commands/rotation.ts
+++ src/commands/rotation.ts
@@ -2,10 +2,11 @@
 import { Client, Message, MessageEmbed } from 'discord.js'
 import axios from 'axios'
 import ChampionMap from '../common/ChampionMap'
+import config from '../config.json'
 
 const rotation = async (client: Client, msg: Message, args: string[]) => {
 
-  const axiosRes = await axios.get("https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-413f33db-1f77-4e29-9b69-886499d3d083");
+  const axiosRes = await axios.get("https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + config.apikey);
   const freeChampionIds: Array<number> = axiosRes.data.freeChampionIds;
 
   let desc = "";
